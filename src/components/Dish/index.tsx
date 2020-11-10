f875d996687c0a5f768b4c88d12b7d99e4695219
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-11-10 16:04:56 -0300

    feat: make API call

--- src/components/Dish/index.tsx
+++ src/components/Dish/index.tsx
@@ -2,19 +2,34 @@ import React from 'react';
 import { FiEdit3, FiTrash } from 'react-icons/fi';
 
 import dishImg from '../../assets/molho.png';
+import api from '../../service/api';
 
 import { Container } from './styles';
 
-const Dish: React.FC = () => {
+interface IFoodProperty {
+  id: string;
+  name: string;
+  image: string;
+  price: string;
+  description: string;
+  available: boolean;
+}
+
+interface IProps {
+  food: IFoodProperty;
+}
+
+const Dish: React.FC<IProps> = ({ food }: IProps) => {
+
   return (
     <Container>
       <header>
-        <img src={dishImg} alt="dish" />
+        <img src={food.image} alt="dish" />
       </header>
       <section className="body">
-        <h2>Ao molho</h2>
-        <p>Macarrão ao molho branco, fughi e cheiro verde das montanhas.</p>
-        <p className="price">R$ 19.90</p>
+        <h2>{food.name}</h2>
+        <p>{food.description}</p>
+        <p className="price">R$ {food.price}</p>
       </section>
       <section className="footer">
         <div className="icon-container">
@@ -34,7 +49,7 @@ const Dish: React.FC = () => {
         </div>
 
         <div className="availability-container">
-          <p>Disponível</p>
+          <p>{food.available}</p>
 
           <label htmlFor="asfa" className="switch">
             <input type="checkbox" />
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Header from '../../components/Header';
 import Dish from '../../components/Dish';
 
+import api from '../../service/api';
+
 import { DishContainer } from './styles';
 
+interface IFoodsProperty {
+  id: string;
+  image: string;
+  name: string;
+  price: string;
+  description: string;
+  available: boolean;
+}
+
 const Dashboard: React.FC = () => {
+
+  const [foods, setFoods] = useState<IFoodsProperty[]>([]);
+  useEffect(() => {
+    async function loadFoods(): Promise<void> {
+      await api.get('foods').then(res => setFoods(res.data))
+    }
+
+    loadFoods();
+  }, []);
+
   return (
     <>
       <Header />
       <DishContainer>
-        <Dish />
-        <Dish />
-        <Dish />
-        <Dish />
+        {foods &&
+          foods.map(food => (
+            <Dish
+              key={food.id}
+              food={food}
+            />
+          ))}
       </DishContainer>
     </>
   );
