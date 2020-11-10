0a05571ba9158757bca728448170f9cd26a82272
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-11-10 12:57:19 -0300

    feat: create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-function App() {
-  return (
-    <h1>Hello World</h1>
-  );
-}
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+
+const App: React.FC = () => (
+  <>
+    <Header />
+    <GlobalStyle />
+  </>
+);
 
 export default App;
