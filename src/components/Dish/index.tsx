e891d5989c317c303ceee8a069c4b2895e53f19f
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-11-10 20:27:41 -0300

    feat: add food editing components

--- src/components/Dish/index.tsx
+++ src/components/Dish/index.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-import { FiEdit3, FiTrash } from 'react-icons/fi';
 
-// import dishImg from '../../assets/molho.png';
+import { FiEdit3, FiTrash } from 'react-icons/fi';
 
 import { Container } from './styles';
 
-interface IFoodProperty {
-  id: string;
+interface IFoodPlate {
+  id: number;
   name: string;
   image: string;
   price: string;
@@ -15,34 +14,45 @@ interface IFoodProperty {
 }
 
 interface IProps {
-  food: IFoodProperty;
+  food: IFoodPlate;
+  handleDelete: (id: number) => {};
+  handleEditFood: (food: IFoodPlate) => void;
 }
 
-const Dish: React.FC<IProps> = ({
-  food
+const Food: React.FC<IProps> = ({
+  food,
+  handleDelete,
+  handleEditFood,
 }: IProps) => {
   const [isAvailable, setIsAvailable] = useState(food.available);
 
   async function toggleAvailable(): Promise<void> {
-    // TODO UPDATE STATUS (available)
     setIsAvailable(!isAvailable);
+  }
+
+  function setEditingFood(): void {
+    handleEditFood(food);
   }
 
   return (
     <Container available={isAvailable}>
       <header>
-        <img src={food.image} alt="dish" />
+        <img src={food.image} alt={food.name} />
       </header>
       <section className="body">
         <h2>{food.name}</h2>
         <p>{food.description}</p>
-        <p className="price">R$ {food.price}</p>
+        <p className="price">
+          R$ <b>{food.price}</b>
+        </p>
       </section>
       <section className="footer">
         <div className="icon-container">
           <button
             type="button"
             className="icon"
+            onClick={() => setEditingFood()}
+            data-testid={`edit-food-${food.id}`}
           >
             <FiEdit3 size={20} />
           </button>
@@ -50,16 +60,24 @@ const Dish: React.FC<IProps> = ({
           <button
             type="button"
             className="icon"
+            onClick={() => handleDelete(food.id)}
+            data-testid={`remove-food-${food.id}`}
           >
             <FiTrash size={20} />
           </button>
         </div>
 
         <div className="availability-container">
-          <p>{food.available}</p>
+          <p>{isAvailable ? 'Disponível' : 'Indisponível'}</p>
 
-          <label htmlFor="asfa" className="switch">
-            <input type="checkbox" />
+          <label htmlFor={`available-switch-${food.id}`} className="switch">
+            <input
+              id={`available-switch-${food.id}`}
+              type="checkbox"
+              checked={isAvailable}
+              onChange={toggleAvailable}
+              data-testid={`change-status-food-${food.id}`}
+            />
             <span className="slider" />
           </label>
         </div>
@@ -68,4 +86,4 @@ const Dish: React.FC<IProps> = ({
   );
 };
 
-export default Dish;
+export default Food;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -6,6 +6,7 @@ import api from '../../service/api';
 
 import { DishContainer } from './styles';
 import ModalAddFood from '../../components/ModalAddFood';
+import ModalEditFood from '../../components/ModalEditFood';
 
 interface IFoodsProperty {
   id: string;
@@ -16,8 +17,20 @@ interface IFoodsProperty {
   available: boolean;
 }
 
+interface IFoodPlate {
+  id: number;
+  name: string;
+  image: string;
+  price: string;
+  description: string;
+  available: boolean;
+}
+
 const Dashboard: React.FC = () => {
+  const [foods, setFoods] = useState<IFoodPlate[]>([]);
+  const [editingFood, setEditingFood] = useState<IFoodPlate>({} as IFoodPlate);
   const [modalOpen, setModalOpen] = useState(false);
+  const [editModalOpen, setEditModalOpen] = useState(false);
 
   const toggleModal = useCallback(() => {
     setModalOpen(!modalOpen);
@@ -27,7 +40,6 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodsProperty, 'id' | 'available'>,
   ): Promise<void> {
     try {
-      // TODO ADD A NEW FOOD PLATE TO THE API
       const id = foods.length + 1;
 
       const updatedFood = { id, ...food, available: true };
@@ -40,7 +52,28 @@ const Dashboard: React.FC = () => {
     }
   }
 
-  const [foods, setFoods] = useState<IFoodsProperty[]>([]);
+  async function handleUpdateFood(
+    food: Omit<IFoodPlate, 'id' | 'available'>,
+  ): Promise<void> {
+    const { id } = editingFood;
+
+    const updatedFood = { id, ...food, available: true };
+
+    const response = await api.put(`/foods/${editingFood.id}`, updatedFood);
+
+    const updatedState = foods.filter(item => item.id !== id);
+
+    setFoods([...updatedState, response.data]);
+  }
+
+  async function handleDeleteFood(id: number): Promise<void> {
+    await api.delete(`/foods/${id}`);
+
+    const updatedState = foods.filter(food => food.id !== id);
+
+    setFoods(updatedState);
+  }
+
   useEffect(() => {
     async function loadFoods(): Promise<void> {
       await api.get('foods').then(res => setFoods(res.data))
@@ -48,6 +81,16 @@ const Dashboard: React.FC = () => {
 
     loadFoods();
   }, []);
+
+  function toggleEditModal(): void {
+    setEditModalOpen(!editModalOpen);
+  }
+
+  function handleEditFood(food: IFoodPlate): void {
+    // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food);
+    setEditModalOpen(true);
+  }
 
   return (
     <>
@@ -57,12 +100,20 @@ const Dashboard: React.FC = () => {
         setIsOpen={toggleModal}
         handleAddFood={handleAddFood}
       />
+      <ModalEditFood
+        isOpen={editModalOpen}
+        setIsOpen={toggleEditModal}
+        editingFood={editingFood}
+        handleUpdateFood={handleUpdateFood}
+      />
       <DishContainer>
         {foods &&
           foods.map(food => (
             <Dish
               key={food.id}
               food={food}
+              handleDelete={handleDeleteFood}
+              handleEditFood={handleEditFood}
             />
           ))}
       </DishContainer>
